716b6ee1fefe00c981cad4cdd78780034a9a2ef2
Grayson Hay <grayson@codesee.io>
2021-06-28 16:23:34 -0700

    Fixing upload parameters

--- src/action.js
+++ src/action.js
@@ -3,94 +3,102 @@ const exec = require("@actions/exec");
 const simpleGit = require("simple-git");
 
 function setupEnv() {
-    core.setSecret('api_token');
+  core.setSecret("api_token");
 }
 
 function getConfig() {
-    const apiToken = core.getInput('api_token', { required: true });
-    const webpackConfigPath = core.getInput('webpack_config_path', {required: false });
-    const supportTypescript = core.getBooleanInput('support_typescript', { required: false });
+  const apiToken = core.getInput("api_token", { required: true });
+  const webpackConfigPath = core.getInput("webpack_config_path", {
+    required: false,
+  });
+  const supportTypescript = core.getBooleanInput("support_typescript", {
+    required: false,
+  });
 
-    return {
-        apiToken,
-        webpackConfigPath: webpackConfigPath === '__NULL__' ? undefined : webpackConfigPath,
-        supportTypescript,
-    }
+  return {
+    apiToken,
+    webpackConfigPath:
+      webpackConfigPath === "__NULL__" ? undefined : webpackConfigPath,
+    supportTypescript,
+  };
 }
 
 async function getRepoOrigin() {
-    const git = simpleGit(".");
-    const remotes = await git.getRemotes(true);
-    const maybeOrigin = remotes.filter(r => r.name === 'origin');
-    if (maybeOrigin.length !== 1) {
-        throw new Error("Unable to determine origin remote")
-    }
+  const git = simpleGit(".");
+  const remotes = await git.getRemotes(true);
+  const maybeOrigin = remotes.filter((r) => r.name === "origin");
+  if (maybeOrigin.length !== 1) {
+    throw new Error("Unable to determine origin remote");
+  }
 
-    const originFullUrl = maybeOrigin[0].refs.fetch || maybeOrigin[0].refs.push;
+  const originFullUrl = maybeOrigin[0].refs.fetch || maybeOrigin[0].refs.push;
 
-    // origins from github look like either `git@github.com:<owner name>/<repo name>`
-    // or https://github.com/<owner name>/<repo name>`
-    // and we only care about owner name and repo name to be able to find their
-    // records
-    const origin = originFullUrl.replace('git@github.com:', '').replace('https://github.com/', '');
-    core.info(`Using github repo ${origin} for the origin`)
-    return origin;
+  // origins from github look like either `git@github.com:<owner name>/<repo name>`
+  // or https://github.com/<owner name>/<repo name>`
+  // and we only care about owner name and repo name to be able to find their
+  // records
+  const origin = originFullUrl
+    .replace("git@github.com:", "")
+    .replace("https://github.com/", "");
+  core.info(`Using github repo ${origin} for the origin`);
+  return origin;
 }
 
 async function installCodesee() {
-    return await exec.exec("yarn", ["add", "codesee"]);
+  return await exec.exec("yarn", ["add", "codesee"]);
 }
 
 async function runCodeseeMap(config) {
-    const args = [
-        "map",
-        "-o",
-        "codesee.map.json",
-    ];
+  const args = ["map", "-o", "codesee.map.json"];
 
-    if (config.webpackConfigPath) {
-        args.push("-w", config.webpackConfigPath);
-    }
-    if (config.supportTypescript) {
-        args.push("--typescript");
-    }
-    const runExitCode = await exec.exec("node_modules/.bin/codesee", args)
+  if (config.webpackConfigPath) {
+    args.push("-w", config.webpackConfigPath);
+  }
+  if (config.supportTypescript) {
+    args.push("--typescript");
+  }
+  const runExitCode = await exec.exec("node_modules/.bin/codesee", args);
 
-    return runExitCode;
+  return runExitCode;
 }
 
 async function runCodeseeMapUpload(config, origin) {
-    const args = [
-        "upload",
-        "--map",
-        "--repo", origin,
-        config.apiToken,
-        "codesee.map.json",
-    ];
+  const args = [
+    "upload",
+    "--type",
+    "map",
+    "--repo",
+    origin,
+    "-a",
+    config.apiToken,
+    "codesee.map.json",
+  ];
 
-    const runExitCode = await exec.exec("node_modules/.bin/codesee", args)
+  const runExitCode = await exec.exec("node_modules/.bin/codesee", args);
 
-    return runExitCode;
-
+  return runExitCode;
 }
 
 async function main() {
-    core.startGroup('Setup');
-    setupEnv();
-    const config =  getConfig();
-    core.debug('CONFIG: ');
-    core.debug(config);
+  core.startGroup("Setup");
+  setupEnv();
+  const config = getConfig();
+  core.debug("CONFIG: ");
+  core.debug(config);
 
-    await core.group('Install codesee-cli', installCodesee);
-    const origin = await core.group('Get Repo Origin', getRepoOrigin);
-    core.endGroup();
+  await core.group("Install codesee-cli", installCodesee);
+  const origin = await core.group("Get Repo Origin", getRepoOrigin);
+  core.endGroup();
 
-    await core.group('Generate Map Data', async () => runCodeseeMap(config))
-    await core.group('Upload Map to Codesee Server', async () => runCodeseeMapUpload(config, origin))
+  await core.group("Generate Map Data", async () => runCodeseeMap(config));
+  await core.group("Upload Map to Codesee Server", async () =>
+    runCodeseeMapUpload(config, origin)
+  );
 }
 
-main().then(() => {
-}).catch(err => {
+main()
+  .then(() => {})
+  .catch((err) => {
     core.setFailed(`CodeSee Map failed: ${err}
-    ${err.stack}`);    
-})
+    ${err.stack}`);
+  });
