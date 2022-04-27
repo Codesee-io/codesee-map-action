a59bf90a1e34c24ae6712407080b6ccbb98b5d30
Daniel C. Silverstein <cubes@codesee.io>
2022-04-27 11:01:31 -0700

    ENG-455: Pass the languages option when calling the map generate command.

--- src/action.js
+++ src/action.js
@@ -182,6 +182,9 @@ async function runCodeseeMap(config, excludeLangs) {
   if (config.apiToken) {
     args.push("-a", config.apiToken);
   }
+  if (config.languages) {
+    args.push("--languages", `'${JSON.stringify(config.languages)}'`);
+  }
 
   args.push("-r", `https://github.com/${config.origin}`);
 
